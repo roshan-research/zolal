bd92cc51fbd17ad7fcd7648d283e5056e7a5f204
Alireza <alireza@Alireza-Ubuntu.(none)>
2012-12-03 18:35:19 +0330

    add app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+var Aye = Backbone.Model.extend({});
+
+var Quran = Backbone.Collection.extend({
+	model: Aye
+});
+
+var AyeView = Backbone.View.extend({
+	template: '<span rel="<%= soure %>-<%= num %>"><%= text %></span>',
+	render: function () {
+		var tmpl = _.template(this.template);
+		this.setElement(tmpl(this.model.toJSON()));
+		return this;
+	}
+});
+
+var QuranView = Backbone.View.extend({
+	el: $("#quran"),
+	initialize: function() {
+		this.collection = new Quran(ayes);
+		this.render();
+	},
+	render: function() {
+		page = _.filter(this.collection.models, function (item) {
+			return item.get('soure') === '20';
+		});
+
+		_.each(page, function (item) {
+			var ayeView = new AyeView({model: item});
+			this.$el.append(ayeView.render().el);
+		}, this);
+	},
+});
+
+var quran = new QuranView();
+
+});
