6960b4efc0510efd1d629f795e414aac3aa41bd4
Hadi <hadi@Hadis-MacBook.local>
2013-07-03 19:38:12 +0430

    change default language

--- zolal.js
+++ zolal.js
@@ -1,6 +1,6 @@
 var store = false;
 var server = '/';
-var language = 'ar';
+var language = 'fa';
 
 var numchars = {'0': '۰', '1': '۱', '2': '۲', '3': '۳', '4': '۴', '5': '۵', '6': '۶', '7': '۷', '8': '۸', '9': '۹'};
 var refine = function(str) {
