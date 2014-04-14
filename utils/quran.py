21f4998638d185f9dd9c5f7a06f1c85419207c49
Alireza Nourian <alireza.nournia@gmail.com>
2014-04-14 16:26:04 +0430

    replace yah character in quran tafsir

--- utils/quran.py
+++ utils/quran.py
@@ -64,5 +64,5 @@ def read_lines(lines, ayas):
 
 def simple_aya(aya):
 	aya = re.sub('[۞۩' + 'ۖۗۚۛۙۘۢ' + 'ًٌٍَُِّْٓ' + 'ٰۥۦ' + ']', '', aya)
-	aya = aya.replace('ي', 'ی').replace('ك', 'ک').replace('ٱ', 'ا').replace('ٔ', 'ئ').replace('یٰ', 'ا')
+	aya = aya.replace('ي', 'ی').replace('ك', 'ک').replace('ٱ', 'ا').replace('ٔ', 'ئ').replace('یٰ', 'ا').replace('ۧ', 'ی')
 	return aya
