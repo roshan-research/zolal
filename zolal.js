fe79abdaa7cae926924257d37810b1be1532ad93
Alireza Nourian <alireza.nournia@gmail.com>
2014-06-08 21:47:33 +0430

    update quran font

--- zolal.js
+++ zolal.js
@@ -165,7 +165,7 @@ var AyaView = Backbone.View.extend({
 		data = this.model.toJSON();
 		text = data['text'];
 		text = text;//.replace(/َٰ/g, 'ٰ').replace(/[۪۫]/g, ''); // font refinement
-		data['html'] = text.replace(/[ ]*([ۖۗۚۛۙۘ])[ ]*/g, '<span class="mark">\$1 </span>');
+		data['html'] = text.replace(/[ ]*([ۖۗۚۛۙۘ])[ ]*/g, '<span class="mark">\$1</span> ');
 		this.setElement(this.template(data));
 		return this;
 	},
