6163d948fcb44ec50c7f8359a41001a7e117c32f
Alireza Nourian <az.nourian@gmail.com>
2014-11-20 06:52:37 +0330

    use s3 for files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var app;
 var store = false;
-var server = 'http://zolal-files.ap01.aws.af.cm/';
+var server = 'http://zolal.s3-website-us-east-1.amazonaws.com/';
 var chrome_app = 'chrome' in window && 'app' in chrome && chrome.app.isInstalled;
 var android_app = 'cordova' in window;
 var searchResultChars = android_app ? 25 : 50;
